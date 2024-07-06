1abac1f8cd6a14131e4d43e5a38cac7872947d50
butuzzz <99124291+butuzzz@users.noreply.github.com>
2024-07-06 13:50:59 +0300

    views.py

--- my_site/my_site/views.py
+++ my_site/my_site/views.py
@@ -59,24 +59,6 @@ def home(request):
     <h1>Рекомендательная инвестиционная платформа</h1>
     <h3>Зарабатывайте на акциях, облигациях и ЦФА российских эмитентов</h3> 
   </div>
-
-  <div class="row mt-5">
-    <div class="col-sm-4">
-      <h3>Column 1</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    </div>
-    <div class="col-sm-4">
-      <h3>Column 2</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    </div>
-    <div class="col-sm-4">
-      <h3>Column 3</h3>        
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    </div>
-  </div>
 </div>
 
 </body>
